8f5c83067d8ccb460c6d6af0c737f167c8b3aa40
Brian Nelson and Connor McCarthy <connor@>
2018-08-16 16:12:04 -0700

    return encoded string back as one string

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
 // business logic
 var encrypt = function (inputString){
   inputString = inputString.replace(/\W/g, '');
-  alert(inputString);
+
   stringLength = inputString.length;
   var squareString = Math.sqrt(stringLength);
   // return (squareString)
   var encryptArr = [];
   var row = Math.ceil(squareString);
   var column = Math.ceil(stringLength/row);
-  alert(stringLength + " " + row + " " + column);
-for (i = 0; i < column * 5; i+= column){
-  alert(column + " " + i)
-  encryptArr.push(inputString[i]);
-}
+  // alert(stringLength + " " + row + " " + column);
+  // for (index = 0, k=0, j=0; index < Math.ceil(stringLength / 5); index++){
+  //   for (i = 0; i < column * 5; i+= column, k++) {
+  //     if (stringLength < (k * column)) {
+  //       j++;
+  //       k = j;
+  //       alert(i + " " + k + " " + inputString[(k * column)]);
+  //     }
+  //     encryptArr.push(inputString[(k * column)]);
+  //   }
+  //   alert(encryptArr);
+  // }
+  for (i=0, j=0, k=0; i<=stringLength*column; i+=column, k+=column) {
+    if (k > stringLength) {
+      j++;
+      k = j;
+    }
+    encryptArr.push(inputString[k % stringLength]);
+  }
   return(encryptArr.join(''));
 
 }
